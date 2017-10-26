58df6aa34e68ace3097a967b636fe2bfd85c6e61
Kim JinSeok <jskim2code@gmail.com>
2017-10-26 09:38:37 +0900

    10th PostForm(forms.Form)

--- instagramlike/post/views.py
+++ instagramlike/post/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from post.forms import PostForm
 from post.models import Post
 
 
@@ -12,15 +13,23 @@ def post_list(request):
     )
 
 def post_create(request):
-    photo = request.FILES.get('photo')
-    if request.method == 'POST' and photo:
+    # photo = request.FILES.get('photo')
+    if request.method == 'POST':
         # print(request.POST)
         ## < QueryDict: {'csrfmiddlewaretoken': ['3bP8vMXgDmwdOlxOv5qb7iwcBDzZyvv94tBUFWTSl2MsQNXHcEeRJ8Au5tWwBGMZ']} >
         # print(request.FILES)
         ## < MultiValueDict: {'photo': [ < InMemoryUploadedFile: 디아블로 - 티리엘.gif(image / gif) >]} >
-        photo = request.FILES['photo']
-        post = Post.objects.create(photo=photo)
-        return HttpResponse(f'<img src="{ post.photo.url }">')
+
+        # photo = request.FILES['photo']
+        # post = Post.objects.create(photo=photo)
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            print(form.cleaned_data)
+            post = Post.objects.create(
+                photo=form.cleaned_data['photo'])
+            return HttpResponse(f'<img src="{ post.photo.url }">')
+        else:
+            return HttpResponse('Form invalid!')
     else:
         return render(
             request,
