e5650fb2380d3a88739c80ef34861cebac31d022
Kim JinSeok <jskim2code@gmail.com>
2017-10-26 09:27:56 +0900

    9th post instance: request.FILES['photo']

--- instagramlike/post/views.py
+++ instagramlike/post/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from post.models import Post
@@ -11,10 +12,16 @@ def post_list(request):
     )
 
 def post_create(request):
-    if request.method == 'POST':
-        print(request.POST)
-        print(request.FILES)
-    elif request.method == 'GET':
+    photo = request.FILES.get('photo')
+    if request.method == 'POST' and photo:
+        # print(request.POST)
+        ## < QueryDict: {'csrfmiddlewaretoken': ['3bP8vMXgDmwdOlxOv5qb7iwcBDzZyvv94tBUFWTSl2MsQNXHcEeRJ8Au5tWwBGMZ']} >
+        # print(request.FILES)
+        ## < MultiValueDict: {'photo': [ < InMemoryUploadedFile: 디아블로 - 티리엘.gif(image / gif) >]} >
+        photo = request.FILES['photo']
+        post = Post.objects.create(photo=photo)
+        return HttpResponse(f'<img src="{ post.photo.url }">')
+    else:
         return render(
             request,
             'post/post_create.html'
