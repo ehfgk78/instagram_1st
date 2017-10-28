eeb3333a224bf83ecf01a163859d79cb4a37f95e
Kim JinSeok <jskim2code@gmail.com>
2017-10-28 21:15:46 +0900

    34th UserCreationForm 재정의 및 UserAdmin - 여기 어려움

--- instagramlike/member/forms.py
+++ instagramlike/member/forms.py
@@ -4,39 +4,42 @@ from django.contrib.auth import (
     authenticate,
     login as django_login,
 )
+from django.contrib.auth.forms import UserCreationForm
 
 User = get_user_model()
 
 
-class SignupForm(forms.Form):
-    username = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
+class SignupForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # 초기화 과정에서 attrs를 업데이트할 필드 이름 목록
+        class_update_fields = ('password1', 'password2')
+        for field in class_update_fields:
+            self.fields[field].widget.attrs.update({
                 'class': 'form-control',
-            }
+            })
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'password1',
+            'password2',
+            'img_profile',
+            'age',
         )
-    )
-    password = forms.CharField(
-        widget=forms.PasswordInput(
-            attrs={
-                'class': 'form-control'
-            }
-        )
-    )
-    password2 = forms.CharField(
-        widget=forms.PasswordInput(
-            attrs={
-                'class': 'form-control',
-            }
-        )
-    )
-    age = forms.IntegerField(
-        widget=forms.NumberInput(
-            attrs={
-                'class': 'form-control',
-            }
-        )
-    )
+        widgets = {
+            'username': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                }
+            ),
+            'age': forms.NumberInput(
+                attrs={
+                    'class': 'form-control',
+                }
+            ),
+        }
 
     # 중복 유저 검사
     def clean_username(self):
@@ -68,6 +71,10 @@ class SignupForm(forms.Form):
 
 
 class LoginForm(forms.Form):
+    """
+    is_valid()에서 주어진 username/password를 사용한 authenticate실행
+    성공시 login(request)메서드를 사용할 수 있음
+    """
     username = forms.CharField(
         widget=forms.TextInput(
             attrs={
--- instagramlike/post/forms.py
+++ instagramlike/post/forms.py
@@ -20,6 +20,7 @@ class PostForm(forms.ModelForm):
         # save()에 author키워드 인수값을 전달할 수 있도록 save()메서드 재정의
         # 새로 저장하려는 instance는 pk값이 없다.
         if not self.instance.pk and commit:
+            # author값을 키워드인수 묶음에서 pop으로 삭제하며 값을 가져온다
             author = kwargs.pop('author', None)
             if not author:
                 raise ValueError('Author field is required')
